c876f432bada6e5ab80e0c368327bb189813ad70
emre pasa <emre.pasa@hva.nl>
2025-10-24 01:18:34 +0200

    api connectie met nextcloud is geregeld

--- app/settings.py
+++ app/settings.py
@@ -3,3 +3,9 @@ from os import environ
 API_KEY = environ.get("API_KEY")
 API_URL = environ.get("API_URL")
 DATABASE = environ.get("DATABASE")
+
+# Nextcloud configuratie
+NEXTCLOUD_SERVER_URL = "https://kai.nl.tab.digital"
+NEXTCLOUD_USERNAME = "emre.pasa@hva.nl"
+NEXTCLOUD_PASSWORD = "Emre@@2005"
+NEXTCLOUD_FOLDER = "EuroVault"  # Folder naam in Nextcloud
