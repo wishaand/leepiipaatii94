4e42611e4515660b878ac873d847652ab11dba1f
wishaand.matai <wishaand.matai@hva.nl>
2026-01-04 16:52:02 +0100

    changing

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import os
+"""Main Flask application entry point"""
+
+from app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=5001)
+
 
